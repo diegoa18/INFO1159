21d2e872b8ebf9768c4b85e34d57797488a4c837
Joaco <canteroxx92@gmail.com>
2026-05-11 20:48:56 -0400

    fix: se corrige un poco el fuerza bruta y le quite sympy(pendiente) x temas de optimizacion

--- NLP/main.py
+++ NLP/main.py
@@ -1,18 +1,19 @@
 import numpy as np
-from sympy import symbols, sympify
 
-x, y = symbols('x y')
-teta = 0.01 # salto de valores
-dx = np.arange(0, 5, teta) 
+# añadir sympy (lo quite pa optimizar y no exploque mi pc :c)
+# da los mismos resultados que el del diego, pero el suyo más rapido, no se por qué xd
+
+teta = float(input("Ingrese el valor de teta: "))
+salto = float(input("Ingrese el valor del salto: "))
+dx = np.arange(0, 5000+salto, salto) 
 
 tupla = []
-expr = sympify(1.20*x + 1.16*y - teta*(2*x**2 + y**2 + (x+y)**2))
 
 #recorre todos los puntos posibles
 for x1 in dx:
     for x2 in dx:
-        if x1 + x2 <= 5: #restricción
-            valor = float(expr.subs({x: x1, y: x2})) #subs, sustituye los valores x e y
+        if x1 + x2 <= 5000: #restricción
+            valor = (1.20*x1 + 1.16*x2 - teta*(2*x1**2 + x2**2 + (x1+x2)**2))
             tupla.append(([x1, x2], valor)) # añade el punto y su valor
 
 valores = [t[1] for t in tupla] # extrae los valores de la función            

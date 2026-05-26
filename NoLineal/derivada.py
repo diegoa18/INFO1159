0d20dfa2c9015ba922cabc1462053eccae3465ae
diegoa18 <diegoanthony.ac1812@gmail.com>
2026-05-26 15:37:18 -0400

    feat: algoritmo find_delta terminado y aislamiento de variables de las funciones

--- NoLineal/derivada.py
+++ NoLineal/derivada.py
@@ -1,31 +1,43 @@
 import sympy as sp
 
 
-def def_derivate():
-    x = sp.symbols("x")
-    func = sp.sympify(input("ingrese f(x): "))
-    x_val, deltax = float(input("ingrese el punto x: ")), float(input("ingrese Δx: "))
+def d_numerica(x, func, x_val, deltax):
     d_numerica = (func.subs(x, x_val + deltax) - func.subs(x, x_val)) / deltax
     print(f"resultado: {sp.N(d_numerica)}")
 
 
-def find_delta():
-    eps_maq = 2.22e-16
-    x = sp.symbols("x")
-    func = sp.sympify(input("ingrese f(x): "))
-    x_val, tolerancia = (
-        float(input("ingrese el punto x: ")),
-        float(input("ingrese la tolerancia e: ")),
-    )
+def find_delta(x, func, x_val, tolerancia, divisiones):
     derivada = sp.diff(func, x)
-    fp = float(derivada.subs(x, x_val))
+    delta = sp.sqrt(tolerancia) * x_val
+    d_num = float(derivada.subs(x, x_val))
 
-    delta = sp.sqrt(eps_maq) * max(abs(x_val), 1.0)
-
-    while (
-        abs(fp - float((func.subs(x, x_val + delta) - func.subs(x, x_val)) / delta))
-        >= tolerancia
+    while not (
+        abs(d_num - float((func.subs(x, x_val + delta) - func.subs(x, x_val)) / delta))
+        < tolerancia
     ):
-        delta /= 2
+        delta /= divisiones
+    print(f"Δx: {sp.N(abs(delta))}")
 
-    print(f"Δx: {sp.N(delta)}")
+
+if __name__ == "__main__":
+    x = sp.symbols("x")
+    caso = input("1: derivada numerica; 2: encontrar delta")
+    if caso == "1":
+        func = sp.sympify(input("ingrese f(x): "))
+        x_val, deltax = (
+            float(input("ingrese el punto x: ")),
+            float(input("ingrese Δx: ")),
+        )
+        d_numerica(x, func, x_val, deltax)
+
+    elif caso == "2":
+        func = sp.sympify(input("ingrese f(x): "))
+        x_val, tolerancia, divisiones = (
+            float(input("ingrese el punto x: ")),
+            float(input("ingrese la tolerancia e: ")),
+            int(input("ingrese divisor: ")),
+        )
+        find_delta(x, func, x_val, tolerancia, divisiones)
+
+    else:
+        print("caso no valido")
--- NoLineal/gradiente.py
+++ NoLineal/gradiente.py
@@ -1,16 +1,7 @@
 import sympy as sp
 
 
-def gradiente_numerico():
-    variables = tuple(sp.symbols(input("ingrese las variables (espacios): ")))
-    func = sp.sympify(input("ingrese f(x): "))
-    x_vals = list(map(float, input("ingrese el punto x (coma): ").split(",")))
-    deltas = list(map(float, input("ingrese los Δx (coma): ").split(",")))
-
-    if not (len(variables) == len(x_vals) == len(deltas)):
-        print("cantidad incompatible de datos")
-        return
-
+def gradiente_numerico(variables, func, x_vals, deltas):
     punto = {v: xv for v, xv in zip(variables, x_vals)}
     f_x = float(func.subs(punto))
 
@@ -21,3 +12,12 @@ def gradiente_numerico():
         grad.append((float(func.subs(punto_pert)) - f_x) / d)
 
     print(f"gradiente: {grad}")
+
+
+if __name__ == "__main__":
+    variables = tuple(sp.symbols(input("ingrese las variables (espacios): ")))
+    func = sp.sympify(input("ingrese f(x): "))
+    x_vals = list(map(float, input("ingrese el punto x (coma): ").split(",")))
+    deltas = list(map(float, input("ingrese los Δx (coma): ").split(",")))
+
+    gradiente_numerico(variables, func, x_vals, deltas)

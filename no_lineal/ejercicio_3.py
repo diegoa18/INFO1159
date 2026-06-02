30cdafc866235d3ac99a41ba6d3aafa30be7b7c6
danirc2024 <dromero2024@alu.uct.cl>
2026-06-02 00:10:36 -0400

    fix: algoritmo de derivadas

--- no_lineal/ejercicio_3.py
+++ no_lineal/ejercicio_3.py
@@ -2,55 +2,33 @@ import sympy as sp
 
 x = sp.symbols('x')
 
-funcion = sp.sympify(input("Ingresa la función f(x): "))
-valor_x = float(input("Ingresa el valor de x donde evaluar: "))
-epsilon = float(input("Ingresa el valor del error absoluto (ej: 0.001): "))
+def derivada_num(funcion, punto_x, delta_x ):
+    return (funcion(punto_x + delta_x) - funcion(punto_x)) / delta_x
 
-if epsilon <= 0:
-    print("Epsilon debe ser mayor que 0")
-    exit()
+def encontrar_deltax(funcion, f,  punto_x, epsilon, divisiones):
+    derivada = sp.diff(funcion, x)
+    delta_x = float(sp.sqrt(epsilon) * punto_x)
+    d_num = derivada_num(f, punto_x, delta_x)
 
-f = sp.lambdify(x, funcion, 'numpy')
+    while not (abs(float(derivada.subs(x, punto_x)) - d_num) < epsilon):
+        delta_x /= divisiones
+        d_num = derivada_num(f, punto_x, delta_x)
 
-derivada_simbolica = sp.diff(funcion, x)
-f_derivada_exacta = sp.lambdify(x, derivada_simbolica, 'numpy')
-valor_derivada_exacta = f_derivada_exacta(valor_x)
-""" 
-def encontrar_derivada_numerica(f, valor_x, delta_x):
-    return (f(valor_x + delta_x) - f(valor_x)) / delta_x """
+    return abs(delta_x)
 
-# 5. Algoritmo de Búsqueda Binaria para encontrar Delta X
-def buscar_delta_x(f, valor_x, valor_exacto, epsilon):
-    bajo = 1e-15 
-    alto = 1.0   
-    
-    delta_x_optimo = None
-    max_iteraciones = 100
-    
-    for _ in range(max_iteraciones):
-        medio = (bajo + alto) / 2
-        
-        derivada_num = f_derivada_exacta(valor_x) 
-        
-        error_absoluto = abs(valor_exacto - derivada_num)
-        
-        if error_absoluto < epsilon:
-            delta_x_optimo = medio
-            bajo = medio
-        else:
-            alto = medio
-            
-        if abs(alto - bajo) < 1e-16:
-            break
-            
-    return delta_x_optimo
+if __name__ == "__main__":
+    caso = input("1: derivada numerica; 2: encontrar delta: ")
+    funcion = sp.sympify(input("ingrese f(x): "))
+    f = sp.lambdify(x, funcion, 'numpy')
 
-delta_x_encontrado = buscar_delta_x(f, valor_x, valor_derivada_exacta, epsilon)
-
-print(f"Derivada exacta en x={valor_x}: {valor_derivada_exacta}")
-if delta_x_encontrado is not None:
-    print(f"Valor de Delta x encontrado: {delta_x_encontrado:.1e}")
-    print(f"Derivada numérica aproximada: {f_derivada_exacta(valor_x)}")
-    print(f"Error real obtenido: {abs(valor_derivada_exacta - f_derivada_exacta(valor_x))}")
-else:
-    print("No se pudo encontrar un Delta x que cumpla con ese nivel de error.")
+    if caso == "1":
+        punto_x = float(input("ingrese el punto x: "))
+        delta_x = float(input("ingrese delta_x: "))
+        print(derivada_num(f, punto_x, delta_x))
+    elif caso == "2":
+        punto_x = float(input("ingrese el punto x: "))
+        epsilon = float(input("ingrese la tolerancia e: "))
+        divisiones = 2
+        print(encontrar_deltax(funcion, f,  punto_x, epsilon, divisiones))
+    else:
+        print("caso no valido")
